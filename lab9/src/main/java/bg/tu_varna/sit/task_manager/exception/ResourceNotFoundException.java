df7524ce23114b7560120fa06815c77af065e081
g-dimitrova <g.dimitrova@tuvarnabg.onmicrosoft.com>
2025-04-13 22:38:03 +0300

    Update ResourceNotFoundException.java

--- lab9/src/main/java/bg/tu_varna/sit/task_manager/exception/ResourceNotFoundException.java
+++ lab9/src/main/java/bg/tu_varna/sit/task_manager/exception/ResourceNotFoundException.java
@@ -4,7 +4,7 @@ package bg.tu_varna.sit.task_manager.exception;
  * Добавено в лабораторно упражнение 8
  */
 public class ResourceNotFoundException extends Exception {
-    public ResourceNotFoundException(long id, Class item) {
-        super(String.format("Resource with id = %i from class = %s", id,  item.getName()));
+    public ResourceNotFoundException(long id, Class<?> clazz) {
+        super(String.format("Resource with id = %d from class = %s", id,  clazz.getName()));
     }
 }
