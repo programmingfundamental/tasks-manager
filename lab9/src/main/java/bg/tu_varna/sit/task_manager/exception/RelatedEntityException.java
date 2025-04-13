05b44b0c0ca1b856fc737d664991068339210d70
g-dimitrova <g.dimitrova@tuvarnabg.onmicrosoft.com>
2025-04-13 22:33:36 +0300

    Update RelatedEntityException.java

--- lab9/src/main/java/bg/tu_varna/sit/task_manager/exception/RelatedEntityException.java
+++ lab9/src/main/java/bg/tu_varna/sit/task_manager/exception/RelatedEntityException.java
@@ -4,7 +4,7 @@ package bg.tu_varna.sit.task_manager.exception;
  * Добавено в лабораторно упражнение 9
  */
 public class RelatedEntityException extends Exception {
-    public RelatedEntityException(long id, Class item) {
-        super(String.format("Conflict: %s with ID = %d is still linked to other records and cannot be deleted", id, item.getName()));
+    public RelatedEntityException(long id, Class<?> clazz) {
+        super(String.format("Conflict: %d with ID = %s is still linked to other records and cannot be deleted", id, item.getName()));
     }
 }
